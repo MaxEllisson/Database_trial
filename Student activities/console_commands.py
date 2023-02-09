2fdc5e24a5f6c461cca290c6056fdfeb7dd381fc
Max Ellisson <maxell0910@highgateschool.org.uk>
2023-02-09 10:28:27 +0000

    heyyyy guys

--- Student activities/console_commands.py
+++ Student activities/console_commands.py
@@ -2,11 +2,31 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import Session
 
-from models import Person
+from models import Person, Activity
 
-andrew = Person(first_name="Andrew", last_name="Dales")
+# Create some instances of the Person class
 
-people = [Person(first_name="Chris", last_name="Brolin"), Person(first_name='Vera', last_name="Malcova")]
+people = [Person(first_name="Andrew", last_name="Dales"),
+
+          Person(first_name="Chris", last_name="Brolin"),
+
+          Person(first_name='Vera', last_name="Malcova"),
+
+          ]
+
+chess = Activity(name="Chess")
+
+fives = Activity(name="Fives")
+
+outdoor_ed = Activity(name="Outdoor Ed")
+
+people[0].activities.append(chess)
+
+people[0].activities.append(fives)
+
+people[1].activities.append(outdoor_ed)
+
+people[1].activities.append(fives)
 
 # Connect to the activities database
 
@@ -14,9 +34,7 @@ engine = create_engine('sqlite:///activities.db', echo=True)
 
 # Create a session and add the people to the database
 
-with Session(engine) as sess:
-    sess.add(andrew)
-
-    sess.add_all(people)
-
-    sess.commit()
+# with Session(engine) as sess:
+#     sess.add_all(people)
+#
+#     sess.commit()
--- Student activities/models.py
+++ Student activities/models.py
@@ -1,13 +1,56 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, UniqueConstraint, ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
 
-# Base is called an Abstract Base Class - our SQL Alchemy models will inherit from this class
+from sqlalchemy.orm import relationship
+
+# Base is called an Abstract Base Class - our SQL Alchemy models will inherit from this class 
 
 Base = declarative_base()
 
+# Sets up a link table with activity_id and person_id as foreign keys 
 
-# Sets up a Person table, this references "activities" via the person_activities table
+# Base.metadata is a container object that keeps together many different features of the database 
+
+person_activity = Table('person_activity',
+
+                        Base.metadata,
+
+                        Column('id', Integer, primary_key=True),
+
+                        Column('activity_id', ForeignKey('activity.id')),
+
+                        Column('person_id', ForeignKey('person.id')),
+
+                        UniqueConstraint('activity_id', 'person_id')
+
+                        )
+
+
+# Sets up an Activity table, this references "attendees" via the person_activities table 
+
+class Activity(Base):
+    __tablename__ = 'activity'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    name = Column(String, unique=True, nullable=False)
+
+    attendees = relationship("Person",
+
+                             secondary=person_activity,
+
+                             order_by='(Person.last_name, Person.first_name)',
+
+                             back_populates="activities")
+
+    # Gives a representation of an Activity (for printing out) 
+
+    def __repr__(self):
+        return f"<Activity({self.name})>"
+
+    # Sets up a Person table, this references "activities" via the person_activities table
+
 
 class Person(Base):
     __tablename__ = 'person'
@@ -18,12 +61,20 @@ class Person(Base):
 
     last_name = Column(String, nullable=False)
 
-    # Gives a representation of a Person (for printing out)
+    activities = relationship("Activity",
+
+                              secondary=person_activity,
+
+                              order_by='Activity.name',
+
+                              back_populates="attendees")
+
+    # Gives a representation of a Person (for printing out) 
 
     def __repr__(self):
         return f"<Person({self.first_name} {self.last_name})>"
 
-    # Include a method:
+        # Include a method:
 
     def greeting(self):
         print(f'{self.first_name} says "hello"!')
